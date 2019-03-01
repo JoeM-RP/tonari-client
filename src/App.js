6593ad3d30bbeb15e7c2ea0a000d60963f2e772e
Joe Meyer <joe.meyer-rp@peartheraputics.com>
2019-03-01 15:57:58 -0600

    Tidy

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>隣</Text>
-        <Text>I did it!</Text>
+        <Text>Open up App.js to start working on your app!</Text>
       </View>
     );
   }
