82cb71120073caabb8c9df36a2d969b5c0f8db17
Joe Meyer <joe.meyer-rp@peartheraputics.com>
2019-04-12 16:39:25 -0500

    Fun with UI

--- src/App.js
+++ src/App.js
@@ -1,12 +1,84 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Dimensions, StyleSheet, View } from 'react-native';
+import { Button, Card, Icon, ListItem, SearchBar, Text } from 'react-native-elements';
+import { MapView } from 'expo';
+
+const { width, height } = Dimensions.get('window');
+const list = [
+  {
+    name: 'Bar Farha',
+    icon: 'local-bar',
+    subtitle: 'Bar'
+  },
+  {
+    name: 'Food Truck Friday',
+    icon: 'event',
+    subtitle: 'Activity'
+  },
+]
 
 export default class App extends React.Component {
+  state = {
+    isBusy: false,
+    mapLoaded: false,
+    search: '',
+    region: {
+      longitude: -122,
+      latitude: 37,
+      longitudeDelta: 0.04,
+      latitudeDelta: 0.09,
+    }
+  }
+
+  updateSearch = search => {
+    this.setState({ search });
+  };
+
   render() {
+    const { search } = this.state;
+
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <MapView
+          style={{flex: 1}}
+          region={this.state.region}
+        />
+        <View style={styles.card}>
+          <Text style={styles.textGreeting}>
+            Hello there!
+          </Text>
+          <Text h4 style={styles.textBanner}>
+            What Interests You?
+          </Text>
+          <SearchBar
+            style={{marginBottom: 15}}
+            placeholder="Type Here..."
+            onChangeText={this.updateSearch}
+            value={search}
+            lightTheme
+          />
+          { this.renderTonariList(list) }
+          <Text h4>
+            隣 Near You 
+          </Text>
+          { this.renderTonariList(list) }
+        </View>
       </View>
+    );
+  }
+
+  renderTonariList(items) {
+    return(items.map((l, i) => (
+        <ListItem
+          style={{ marginBottom: 15}}
+          key={i}
+          leftIcon={{ name: l.icon}}
+          title={l.name}
+          subtitle={l.subtitle}
+          chevronColor="white"
+          chevron
+        />
+      ))
     );
   }
 }
@@ -15,7 +87,26 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  card: {
+    position: 'absolute',
+    bottom: 0,
+    left: 10,
+    borderRadius: 20,
+    backgroundColor: 'white',
+    width: width - 20,
+    padding:20,
+  },
+  cardButton: {
+    borderRadius: 0, 
+    marginLeft: 0, 
+    marginRight: 0, 
+    marginBottom: 0
+  },
+  textGreeting: {
+    marginBottom: 15
+  },
+  textBanner: {
+    marginBottom: 15
+  }
 });
---
+++ src/content/home/HomePage.js
@@ -0,0 +1,18 @@
+// Import libraries
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+// Build Class Component
+export default class Home extends Component {
+     render() {
+        return (
+            <View>
+                <Text>I did it!</Text>
+            </View>
+        );
+  }
+}
+
+// Styles
+const styles = {
+}
