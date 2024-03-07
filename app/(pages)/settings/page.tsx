13a90a393b3246c433efedead52062eac3aaa7c6
Joe Meyer <jmeyer@rightpoint.com>
2024-03-07 13:06:38 -0600

    Adds info

--- app/(pages)/settings/page.tsx
+++ app/(pages)/settings/page.tsx
@@ -3,7 +3,7 @@
 export default function Settings() {
     return (
         <section id="page-settings">
-            <div className="w-dvw h-dvh p-4"><p>Settings</p></div>
+            <div className="w-dvw h-dvh p-4"><p>Settings</p><p>{window.location.hostname}</p></div>
         </section>
     )
 }
